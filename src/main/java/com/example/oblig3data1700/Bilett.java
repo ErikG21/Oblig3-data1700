04a31548c24829826410e82741e00b2421fe90ee
ErikG21 <143500444+ErikG21@users.noreply.github.com>
2024-04-22 14:23:03 +0200

    .

--- src/main/java/com/example/oblig3data1700/Bilett.java
+++ src/main/java/com/example/oblig3data1700/Bilett.java
@@ -1,5 +1,15 @@
 package com.example.oblig3data1700;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Bilett {
     private int id;
     private String fornavn;
@@ -8,71 +18,6 @@ public class Bilett {
     private String nummer;
     private String antall;
     private String film;
-
-    public Bilett(int id, String fornavn, String etternavn, String epost, String nummer, String antall, String film) {
-        this.id = id;
-        this.fornavn = fornavn;
-        this.etternavn = etternavn;
-        this.epost = epost;
-        this.nummer = nummer;
-        this.antall = antall;
-        this.film = film;
-    }
-
-    public Bilett() {
-    }
-
-    public int getId() {return id;}
-
-    public void setId(int id) {this.id = id;}
-
-    public String getFornavn() {
-        return fornavn;
-    }
-
-    public void setFornavn(String fornavn) {
-        this.fornavn = fornavn;
-    }
-
-    public String getEtternavn() {
-        return etternavn;
-    }
-
-    public void setEtternavn(String etternavn) {
-        this.etternavn = etternavn;
-    }
-
-    public String getEpost() {
-        return epost;
-    }
-
-    public void setEpost(String epost) {
-        this.epost = epost;
-    }
-
-    public String getNummer() {
-        return nummer;
-    }
-
-    public void setNummer(String nummer) {
-        this.nummer = nummer;
-    }
-
-    public String getAntall() {
-        return antall;
-    }
-
-    public void setAntall(String antall) {
-        this.antall = antall;
-    }
-
-    public String getFilm() {
-        return film;
-    }
-
-    public void setFilm(String film) {
-        this.film = film;
-    }
 }
 
 
